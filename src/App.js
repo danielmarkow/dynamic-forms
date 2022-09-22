5719b12ffaaebd04f180eac8e4b11f02cb3db81e
Markow, Daniel (K-FIMC/2) <daniel.markow@volkswagen.de>
2022-09-22 15:58:51 +0200

    Initial Commit, got dynamic form generation working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [inputFields, setInputFields] = useState({
+    username : {
+      name : "username",
+      type: "text",
+      label: "User Name",
+      value : "",            
+    },
+    useremail : {
+      name : "useremail",
+      type: "email",
+      label : "User Email",
+      value: "",
+    }
+  });
+
+  const onChange = (event) => {
+    setInputFields(
+      {
+        ...inputFields, 
+        [event.target.id]: {...inputFields[event.target.id], value: event.target.value}
+      }
+    );
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputFields);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <hr />
+      <form onSubmit={onSubmit}>
+        {Object.keys(inputFields).map((k) => 
+          <div key={k}>
+            <label htmlFor={k}>{inputFields[k].label}</label>
+            <input  
+              id={k} 
+              name={inputFields[k].name}
+              type={inputFields[k].type}
+              value={inputFields[k].value}
+              onChange={onChange}
+            /> 
+          </div>
+        )}
+        <button type="submit">submit</button>
+      </form>
+    </>
   );
 }
 
