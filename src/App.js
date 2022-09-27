b0eeaccac6bbab413be2daa4cb505a65c001f17b
Daniel Markow <daniel.markow@outlook.com>
2022-09-27 18:33:52 +0200

    attemting dynamic form validation

--- src/App.js
+++ src/App.js
@@ -6,26 +6,29 @@ import DynInput from "./components/DynInput";
 import ConfigForm from "./components/ConfigForm";
 
 function App() {
-  const [inputFields, setInputFields] = useState({
-    username : {
-      name : "username",
-      type: "text",
-      label: "User Name",
-      value : "",            
-    },
-    useremail : {
-      name : "useremail",
-      type: "email",
-      label : "User Email",
-      value: "",
-    }
-  });
+  // username : {
+  //   name : "username",
+  //       type: "text",
+  //       label: "User Name",
+  //       value : "",
+  // },
+  // useremail : {
+  //   name : "useremail",
+  //       type: "email",
+  //       label : "User Email",
+  //       value: "",
+  // }
+
+  const [inputFields, setInputFields] = useState({});
 
   const configSchema = object({
     inputName: string().min(5).required(),
     inputType: string().required(),
     inputLabel: string().min(5).required(),
-  })
+  });
+
+  let dynSchemaConfig = {};
+  let dynSchema = object(dynSchemaConfig);
 
   const configDefaultValue = {
     inputName: "",
@@ -33,12 +36,15 @@ function App() {
     inputLabel : "",
   };
 
-  const [config, setConfig] = useState(configDefaultValue);
-  const [configError, setConfigError] = useState({
+  const configErrorDefaultValue = {
     inputName: "",
     inputType: "",
     inputLabel: "",
-  });
+  };
+
+  // tracks ConfigForm values and validation errors
+  const [config, setConfig] = useState(configDefaultValue);
+  const [configError, setConfigError] = useState(configErrorDefaultValue);
  
   const onChange = (event) => {
     setInputFields(
@@ -51,7 +57,19 @@ function App() {
 
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log(inputFields);
+
+    // generate value object for easier validation of
+    // dynamic form components
+    let formValues = {};
+    Object.entries(inputFields).forEach((entr) => formValues = {...formValues, [entr[0]]: entr[1].value});
+
+    // TODO figure out why validation shows valid when it clearly isn't
+    dynSchema.validate(formValues).then(() => {
+      console.log("valid");
+    }).catch((err) => {
+      console.log(err);
+    });
+    // console.log(formValues);
   };
 
   const onChangeConfig = (event) => {
@@ -62,12 +80,19 @@ function App() {
     event.preventDefault();
 
     configSchema.validate(config, {abortEarly: false}).then(() => {
+      setConfigError(configErrorDefaultValue);
       setInputFields({...inputFields, [config.inputName] : {
           name : config.inputName,
           type : config.inputType,
           label : config.inputLabel,
           value : "",
         }});
+
+      // TODO replace example schema with actual
+      dynSchemaConfig = {...dynSchemaConfig, [config.inputName]: string().min(5).required()};
+      dynSchema = object(dynSchemaConfig);
+      console.log(dynSchemaConfig)
+      console.log(dynSchema)
 
       setConfig(configDefaultValue);
     }).catch((err) => {
